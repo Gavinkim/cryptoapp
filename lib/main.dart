1c15175d39812cf7aaa8d6d7a95f8c7b28bc66bf
Gavin kim <dreamworld612@gmail.com>
2020-07-09 20:33:14 +0900

    bloc apply to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:cryptoapp/blocs/crypto/crypto_bloc.dart';
+import 'package:cryptoapp/repository/crypto_repository.dart';
 import 'package:cryptoapp/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,13 +11,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'CryptoApp',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Colors.black,
-          accentColor: Colors.tealAccent,
-        ),
-        home: HomeScreen());
+    return BlocProvider<CryptoBloc>(
+      create: (_) => CryptoBloc(
+        cryptoRepository: CryptoRepository(),
+      )..add(AppStarted()),
+      child: MaterialApp(
+          title: 'CryptoApp',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primaryColor: Colors.black,
+            accentColor: Colors.tealAccent,
+          ),
+          home: HomeScreen()),
+    );
   }
 }
